335a0fb7dfd33f11f40529b75b3273a2fe483993
tajmim <52608165+tajmim@users.noreply.github.com>
2019-07-06 11:28:29 -0700

    initial commit
    todays first commit of two file named 2nd.c and gf.c

---
+++ 2nd.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i,j,k[0],temp=0;
+    //clrscr();
+    printf("Enter 10 numbers\n");
+
+    for(i=0;i<=9;i++)
+    scanf("%d",&k[i]);
+    for(i=0;i<=8;i++)
+    for(j=i+1;j<=9;j++)
+    {
+        if(k[i]>k[j])
+        {
+            temp=k[i];
+            k[i]=k[j];
+            k[j]=temp;
+        }
+    }
+
+
+printf("\n");
+for(i=0;i<=9;i++)
+printf("%d ",k[i]);
+
+getch();
+return 0;
+}
---
+++ gf.c
@@ -0,0 +1,23 @@
+
+#include<stdio.h>
+
+int main()
+{
+    int i;
+    {
+  for(i=0;i<=100;i++)
+     {
+         if(i%2==0)
+            printf("%d ",i);
+     }
+    }
+{
+    for(i=0;i<=100;i++)
+    {
+        if(i%2!=0)
+            printf("%d ",i);
+    }
+}
+
+     return 0;
+}
